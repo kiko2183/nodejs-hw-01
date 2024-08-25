792a2de5013f9de0f6669882530e32a095691d1b
kiko2183 <romankulicnij@gmail.com>
2024-08-25 18:51:52 +0300

    1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
+
+    for (let i = 0; i < number; i++) {
+      contacts.push(createFakeContact());
+    }
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+    console.log(`${number} contacts generated and added.`);
+  } catch (error) {
+    console.error('Error generating contacts:', error.message);
+  }
+};
 
 generateContacts(5);
